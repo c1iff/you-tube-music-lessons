c3b8d33533a4e228dde7f0f852e5daf79a15defb
Chris Clifford <cclifford82@gmail.com>
2017-03-01 18:35:39 -0800

    add video url upload and index

---
+++ app/models/video.rb
@@ -0,0 +1,18 @@
+class Video < ApplicationRecord
+  YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+
+  validates :link, presence: true, format: YT_LINK_FORMAT
+
+  before_create :check_video
+
+  def check_video()
+    video = Yt::Video.new url: self.link
+    self.uid = video.id
+    self.title = video.title
+    self.likes = video.like_count
+    self.dislikes = video.dislike_count
+    self.published_at = video.published_at
+  rescue Yt::Errors::NoItems
+    self.title = ''
+  end
+end
