5258dc3644dee53f623367d1fd9eebf5496cb3ef
Chris Clifford <cclifford82@gmail.com>
2017-03-06 13:43:36 -0800

    add video player

---
+++ app/assets/javascripts/player.js
@@ -0,0 +1,42 @@
+
+//load api code
+var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//set video id
+var videoId;
+function setVideoId(id) {
+  player.loadVideoById(id)
+}
+//create player
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: videoId,
+    events: {
+      // 'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+//this function called when api is ready
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+//watch for state change
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
